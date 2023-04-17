8e56d17536cbb8ca56b4046f5c43819bd7298a98
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-04-17 14:07:02 -0700

    Update Cheatcodes.t.sol
    add testFail cheatcode, add the warp cheatcode. one allows us to check whether a test fails. and warp allows us to move block.timestamp arbitrarirly

--- test/Cheatcodes.t.sol
+++ test/Cheatcodes.t.sol
@@ -11,4 +11,19 @@ contract CheatcodesTest is Test {
     function setUp() public {
         cheats = new Cheatcodes();
     }
+
+    //we can run a test that that to ask if something will fail(like inverse idk) w/ function testFail..(){}
+    /// this test will fail because our contracts requires us to wait 2 minutes, but the block.timestamp
+    /// is initiated when the test is called. e.g. our block.timestamp will start at 0 when this function is called
+    function testFailSetNumber() public {
+        cheats.setNumber(5);
+    }
+
+    //cheatcode #1 time 
+    function setNumber() public {
+        //foundry cheatcode --increase block.timestamp by x
+        vm.warp(150); 
+
+        cheats.setNumber(5);
+    }
 }
